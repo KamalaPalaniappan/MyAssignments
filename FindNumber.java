55455a0938fee737783c91a4289237f60a17e60e
KamalaPalaniappan <43532584+KamalaPalaniappan@users.noreply.github.com>
2024-04-23 21:38:36 +0530

    Add files via upload
    Day2 Positive and Negative number

---
+++ FindNumber.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		int num = 0;
+		if(num>0)
+		{
+			System.out.println(num +" is a positive number" );
+
+		}
+		else if(num == 0){
+			System.out.println("number is zero");
+		}
+		else
+			System.out.println(num +" is a negative number");
+
+	}
+
+}
