a6761f0703c0baa7df49ffd63848befd24cdc57b
KamalaPalaniappan <43532584+KamalaPalaniappan@users.noreply.github.com>
2024-04-23 22:00:11 +0530

    Add files via upload
    Day 2 DataTypes

---
+++ Firefox.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Firefox {
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String broswerName = "firefox";
+		boolean pageLoaded = true;
+		int noofBrowser = 1998;
+		char browserLogo = 'f';
+		System.out.println("Browserversion:"+browserVersion+"\n"+"browsername is :"+broswerName+"\n"+"visible to :"+isVisible+"\n"+"Releaseyear :"+releaseYear+"\n"+"Browser logo is :"+browserLogo);
+		
+	}
+	
+
+}
